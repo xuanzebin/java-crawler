6cb78667658ec8462357eb1edf023695a5b71826
xuanzebin <xuanzebin@jd.com>
2020-02-21 00:21:11 +0800

    feat: 新增flyway工具，现在可以通过该工具初始化数据库了

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,12 @@
+create table LINKS_TO_BE_PROCESSED(link varchar(2000));
+
+create table LINKS_ALREADY_PROCESSED(link varchar(2000));
+
+create table NEWS(
+id bigint primary key auto_increment,
+link varchar(2000),
+title text,
+content text,
+created_at timestamp,
+update_at timestamp
+);
---
+++ src/main/resources/db/migration/V2__init_data.sql
@@ -0,0 +1,2 @@
+insert into LINKS_TO_BE_PROCESSED (Link)
+values ('https://sina.cn')
